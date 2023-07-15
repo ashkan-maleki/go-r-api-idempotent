ec85115d7080ce696b2df68285d1032d447a9279
ma.maleki <ma.maleki@PC-1293>
2023-07-15 09:05:51 +0330

    setup shipping service and fiber hanlders

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"github.com/ashkan-maleki/go-r-api-idempotent/cmd/config"
+	"github.com/ashkan-maleki/go-r-api-idempotent/internal/db/pg"
+	"github.com/ashkan-maleki/go-r-api-idempotent/internal/handler"
+	"github.com/ashkan-maleki/go-r-api-idempotent/internal/service"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
 
 func main() {
-	fmt.Println("hello")
+	db, err := pg.New(config.PostgresDsn)
+	if err != nil {
+		panic(err)
+	}
+	shippingRepo, err := service.NewShipping(db)
+	if err != nil {
+		panic(err)
+	}
+	shippingHandler := handler.NewShippingHandler(shippingRepo)
+
+	app := fiber.New()
+	app.Post("shipping/order", shippingHandler.Order)
+	log.Fatal(app.Listen(":3000"))
 }
