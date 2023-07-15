1703bb84d95bdf073de9f7e58bfb42866d412295
ma.maleki <ma.maleki@PC-1293>
2023-07-15 08:56:10 +0330

    setup pg

--- internal/db/pg/pg.go
+++ internal/db/pg/pg.go
@@ -1,1 +1,16 @@
 package pg
+
+import (
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func New(postgresDsn string) (*gorm.DB, error) {
+	db, err := gorm.Open(postgres.Open(postgresDsn), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
